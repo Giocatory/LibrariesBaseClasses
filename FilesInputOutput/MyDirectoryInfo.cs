094301e52efbf7e41bbcc8cd44ea63d6d9716aeb
Giocatory <nivolyas@gmail.com>
2022-02-21 13:39:59 +0300

    21.02.2022

---
+++ FilesInputOutput/MyDirectoryInfo.cs
@@ -0,0 +1,36 @@
+﻿namespace FilesInputOutput
+{
+    internal class MyDirectoryInfo
+    {
+
+        public MyDirectoryInfo(string path)
+        {
+            Path = path;
+        }
+        public static string Path { get; set; } = ".";
+
+        // Привязать к текущему каталогу (DirectoryInfo df = new("."))
+        // Bind to current directory (DirectoryInfo df = new("."))
+
+        private readonly DirectoryInfo df = new(Path);
+
+        public void Ls()
+        {
+            foreach (var file in df.GetDirectories())
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        public void PWD()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Full Name: {df.FullName}");
+            Console.WriteLine($"Name: {df.Name}");
+            Console.WriteLine($"Parent: {df.Parent}");
+            Console.WriteLine($"Creation: {df.CreationTime}");
+            Console.WriteLine($"Attributes: {df.Attributes}");
+            Console.WriteLine($"Root: {df.Root}");
+        }
+    }
+}
---
+++ FilesInputOutput/Program.cs
@@ -0,0 +1,15 @@
+﻿using FilesInputOutput;
+
+MyDirectoryInfo mdi = new("D:/localhost/Giocatory/LibrariesBaseClasses/FilesInputOutput");
+mdi.Ls();
+// D:\localhost\Giocatory\LibrariesBaseClasses\FilesInputOutput\bin\Debug\net6 .0\files
+// D:\localhost\Giocatory\LibrariesBaseClasses\FilesInputOutput\bin\Debug\net6 .0\ref
+mdi.PWD();
+/*
+Full Name: D:\localhost\Giocatory\LibrariesBaseClasses\FilesInputOutput\bin\Debug\net6.0
+Name: net6.0
+Parent: D:\localhost\Giocatory\LibrariesBaseClasses\FilesInputOutput\bin\Debug
+Creation: 21.02.2022 11:42:20
+Attributes: Directory
+Root: D:\
+*/
