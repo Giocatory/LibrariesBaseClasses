aa1fddda29eb884a5dc25d481e42e429310ded28
Giocatory <nivolyas@gmail.com>
2022-02-22 19:00:18 +0300

    22.02.2022

---
+++ MyConnectionFactory/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+string? dataProviderString = ConfigurationManager.AppSettings["provider"];
+DataProvider dataProvider = DataProvider.None;
+if (Enum.IsDefined(typeof(DataProvider), dataProviderString))
+{
+    dataProvider = (DataProvider)Enum.Parse(typeof(DataProvider), dataProviderString);
+}
+else
+{
+    Console.WriteLine("Sorry, no provider exists!");
+    return;
+}
+
+IDbConnection myConnection = GetConnection(dataProvider);
+Console.WriteLine($"Connection type: {myConnection.GetType().Name ?? "unrecognized type"}"); // SqlConnection
+
+
+static IDbConnection GetConnection(DataProvider dataProvider)
+{
+    IDbConnection? connection;
+    switch (dataProvider)
+    {
+        case DataProvider.SqlServer:
+            connection = new SqlConnection();
+            break;
+        default:
+            connection = null;
+            break;
+    }
+    return connection;
+}
+
+enum DataProvider
+{
+    SqlServer,
+    None
+}
