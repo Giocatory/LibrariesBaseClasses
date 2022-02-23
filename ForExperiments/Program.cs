7c073ce70356a2f01b5eaf3a4d4a27906e20b00c
Giocatory <nivolyas@gmail.com>
2022-02-23 20:29:29 +0300

    23.02.2022

---
+++ ForExperiments/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+// string connectionString = "Server=.\SQLEXPRESS;Database=master;Trusted_Connection=True;";
+string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+Console.WriteLine(connectionString);
+
+SqlConnection connection = new SqlConnection(connectionString);
+try
+{
+    // Открываем подключение
+    await connection.OpenAsync();
+    Console.WriteLine("Подключение открыто");
+}
+catch (SqlException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    // если подключение открыто
+    if (connection.State == ConnectionState.Open)
+    {
+        // закрываем подключение
+        await connection.CloseAsync();
+        Console.WriteLine("Подключение закрыто...");
+    }
+}
+Console.WriteLine("Программа завершила работу.");
+Console.Read();
